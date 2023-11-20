22f3140cb6311a0e10e171f327a38e891254f707
JuanCompaire <juancompaire2004@gmail.com>
2023-11-20 08:33:06 +0100

    punto 1 casi finish

--- models.py
+++ models.py
@@ -1,1 +1,14 @@
 from odoo import api, models, fields
+
+class Cerveza(models.Model):
+    _name = 'cerveceria.cerveza'
+    _description = 'cerveza.cerveza'
+
+    tipo = fields.Char(string="Tipo")
+    name = fields.Char(string="Nombre")
+    descripcion = fields.Char(string="Descripcion")
+    cantidad_alcohol = fields.Float(string="Cantidad de alcohol (%)")
+    precio_unidad = fields.Float(string="Precio por unidad")
+    volumen_unidad = fields.Float(string="Volumen por unidad (ml)")
+    cantidad_inventario = fields.Integer(string="Cantidad en inventario")
+    disponible = fields.Boolean(string="Disponible")
